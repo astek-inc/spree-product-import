c2c63d7a2271b6bec9609c838ea61682226ab565
Edwin Horneij <edwin@astekwallcovering.com>
2016-02-08 09:48:19 -0800

    Renaming override to resolve conflict.

--- app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
+++ app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
@@ -1,7 +1,7 @@
 
 Deface::Override.new(
   :virtual_path => "spree/layouts/admin",
-  :name => "admin_content_admin_tab_parser",
+  :name => 'add_product_imports_to_admin_menu',
   :insert_bottom => "[data-hook='admin_tabs']",
   :text => "<ul class='nav nav-sidebar'><%= tab :product_imports,  :url => admin_product_imports_url, :icon => 'import' %></ul>",
   :disabled => false
