5e70778e334ca16cfa93cbc576d26a3cd59918ad
Edwin Horneij <edwin@astekwallcovering.com>
2016-02-08 17:00:19 -0800

    Adding error message tooltip if updating import item with error status.

--- app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
+++ app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
@@ -1,8 +1,9 @@
 
 Deface::Override.new(
-  :virtual_path => "spree/layouts/admin",
-  :name => 'add_product_imports_to_admin_menu',
-  :insert_bottom => "[data-hook='admin_tabs']",
-  :text => "<ul class='nav nav-sidebar'><%= tab :product_imports,  :url => admin_product_imports_url, :icon => 'import' %></ul>",
-  :disabled => false
+    :original => 'f75e9ec1ae2ef44ea72f69b18e9ef5b5584580a2',
+    :virtual_path => "spree/layouts/admin",
+    :name => 'add_product_imports_to_admin_menu',
+    :insert_bottom => "[data-hook='admin_tabs']",
+    :text => "<ul class='nav nav-sidebar'><%= tab :product_imports,  :url => admin_product_imports_url, :icon => 'import' %></ul>",
+    :disabled => false
 )
