dfda619ee5685d3a323622b81c7a20a57f7d2207
Edwin Horneij <edwin@astekwallcovering.com>
2016-03-04 10:35:59 -0800

    Populating product_import_image_server_id of existing imports.

--- db/migrate/20160303232203_populate_product_import_image_servers.rb
+++ db/migrate/20160303232203_populate_product_import_image_servers.rb
@@ -6,9 +6,13 @@ class PopulateProductImportImageServers < ActiveRecord::Migration
     ].each do |server|
       Spree::ProductImportImageServer.create(server)
     end
+
+    server = Spree::ProductImportImageServer.find_by(name: 'Brewster')
+    Spree::ProductImport.update_all(product_import_image_server_id: server.id)
   end
 
   def down
+    Spree::ProductImport.update_all(product_import_image_server_id: nil)
     Spree::ProductImportImageServer.destroy_all
   end
 end
