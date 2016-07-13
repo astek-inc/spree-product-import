2dde34b2283c59f6627f46eb8b68e05fba8df0e4
Edwin Horneij <edwin@astekwallcovering.com>
2016-07-13 10:14:28 -0700

    Clarify comment

--- app/models/spree/product_import_item.rb
+++ app/models/spree/product_import_item.rb
@@ -377,10 +377,10 @@ module Spree
 
       unless replacements.nil?
         replacements.split(';').each do |pair|
-          find, replace = pair.split(',', -1) # Enables replacement of a string with an empty string
           if pair.start_with? ','
             raise 'String to find in SKU cannot be empty'
           end
+          find, replace = pair.split(',', -1) # Enables replacement of a string with an empty string (remove characters from SKU)
           sku = sku.sub(find, replace)
         end
       end
