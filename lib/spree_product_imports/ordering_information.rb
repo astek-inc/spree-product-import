e3d9ebb76b9bba7a41b55cc8842be0c555f6e49f
Edwin Horneij <edwin@astekwallcovering.com>
2016-09-22 10:37:20 -0700

    Clarify ordering information for print-to-order items

--- lib/spree_product_imports/ordering_information.rb
+++ lib/spree_product_imports/ordering_information.rb
@@ -4,7 +4,9 @@ module SpreeProductImports
     # Assign items for "Ordering Information" box on product page
     def self.assign product, item_data
 
-        product.set_order_info_item('Please confirm availability -- 3-8 week lead time')
+        if item_data['printtoorder'].nil?
+          product.set_order_info_item('Please confirm availability -- 3-8 week lead time')
+        end
 
         case item_data['type']
           when 'Wallpaper'
@@ -16,6 +18,8 @@ module SpreeProductImports
         end
 
         unless item_data['printtoorder'].nil?
+          product.set_order_info_item('Print to order')
+          product.set_order_info_item('Made to order -- final sale')
           product.set_order_info_item('Unprinted margins')
           product.set_order_info_item('Customization available')
         end
