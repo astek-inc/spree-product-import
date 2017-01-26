bb4c847b939701b886c0900d5858162720539655
Edwin Horneij <edwin@astekwallcovering.com>
2017-01-26 14:51:22 -0800

    Comment out install of unused frontend files

--- lib/generators/spree_product_imports/install/install_generator.rb
+++ lib/generators/spree_product_imports/install/install_generator.rb
@@ -5,12 +5,12 @@ module SpreeProductImports
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_product_imports\n"
+        # append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_product_imports\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_product_imports\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_product_imports\n", :before => /\*\//, :verbose => true
+        # inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_product_imports\n", :before => /\*\//, :verbose => true
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_product_imports\n", :before => /\*\//, :verbose => true
       end
 
