ba2b3876c205bffd8672c7a97451f052ee0ec36d
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:39:12 -0800

    Update original for override

--- app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
+++ app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
@@ -1,9 +1,8 @@
-
 Deface::Override.new(
-    :original => 'f75e9ec1ae2ef44ea72f69b18e9ef5b5584580a2',
-    :virtual_path => "spree/layouts/admin",
-    :name => 'add_product_imports_to_admin_menu',
-    :insert_bottom => "[data-hook='admin_tabs']",
-    :text => "<ul class=\"nav nav-sidebar\"><%= main_menu_tree Spree.t(:product_imports), icon: 'import', sub_menu: 'product_import', url: '#sidebar-product-import' %>",
-    :disabled => false
+    original: '9fb78af45a046627caa5187ab06e4cf1b6bdf986',
+    virtual_path: "spree/layouts/admin",
+    name: 'add_product_imports_to_admin_menu',
+    insert_bottom: "[data-hook='admin_tabs']",
+    text: "<ul class=\"nav nav-sidebar\"><%= main_menu_tree Spree.t(:product_imports), icon: 'import', sub_menu: 'product_import', url: '#sidebar-product-import' %>",
+    disabled: false
 )
