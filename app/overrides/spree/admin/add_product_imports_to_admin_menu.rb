e8d76c691b9ac2cd722aafdba0472e9dbe46dc0d
Edwin Horneij <edwin@astekwallcovering.com>
2017-06-16 15:05:53 -0700

    Add closing ul tag

--- app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
+++ app/overrides/spree/admin/add_product_imports_to_admin_menu.rb
@@ -3,6 +3,6 @@ Deface::Override.new(
     virtual_path: "spree/layouts/admin",
     name: 'add_product_imports_to_admin_menu',
     insert_bottom: "[data-hook='admin_tabs']",
-    text: "<ul class=\"nav nav-sidebar\"><%= main_menu_tree Spree.t(:product_imports), icon: 'import', sub_menu: 'product_import', url: '#sidebar-product-import' %>",
+    text: "<ul class=\"nav nav-sidebar\"><%= main_menu_tree Spree.t(:product_imports), icon: 'import', sub_menu: 'product_import', url: '#sidebar-product-import' %></ul>",
     disabled: false
 )
